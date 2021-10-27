b33c501e0eb127b799c09ead6418323da1de075c
LyesHarrar <37153801+LyesHarrar@users.noreply.github.com>
2021-10-27 11:18:14 +0200

    mise en place des routers, controllers, models ainsi que les routes CRUD

--- app.js
+++ app.js
@@ -1,16 +1,19 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const path = require('path');
 
-const Sauce = require('./models/sauce');
+const sauceRoutes = require('./routes/sauce');
+const userRoutes = require('./routes/user');
 
 const app = express();
 
 //connexion à la base de données
-mongoose.connect('mongodb+srv://lyes:9EzJVLfgHL9G6WWq@cluster0.elrxx.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
-  { useNewUrlParser: true,
-    useUnifiedTopology: true })
-  .then(() => console.log('Connexion à MongoDB réussie !'))
-  .catch(() => console.log('Connexion à MongoDB échouée !'));
+mongoose.connect('mongodb+srv://lyes:9EzJVLfgHL9G6WWq@cluster0.elrxx.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    })
+    .then(() => console.log('Connexion à MongoDB réussie !'))
+    .catch(() => console.log('Connexion à MongoDB échouée !'));
 
 // ajout des headers pour les requêtes
 app.use((req, res, next) => {
@@ -18,32 +21,13 @@ app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
     next();
-  });  
+});
 
 app.use(express.json()); //Utilisé pour parser le corps JSON
 
 // routes
+app.use('/api/sauces', sauceRoutes);
+app.use('/images', express.static(path.join(__dirname, 'images')));
+app.use('/api/auth', userRoutes);
 
-app.use((req,res)=>{
-  const stuff = [
-    {
-      _id: 'oeihfzeoi',
-      title: 'Mon premier objet',
-      description: 'Les infos de mon premier objet',
-      imageUrl: 'https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg',
-      price: 4900,
-      userId: 'qsomihvqios',
-    },
-    {
-      _id: 'oeihfzeomoihi',
-      title: 'Mon deuxième objet',
-      description: 'Les infos de mon deuxième objet',
-      imageUrl: 'https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg',
-      price: 2900,
-      userId: 'qsomihvqios',
-    },
-  ];
-  res.status(200).json(stuff);
-    });
-  
 module.exports = app;
